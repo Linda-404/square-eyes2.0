3eccd612972a517e931f728dd8a9d2da267ab858
Linda-404 <Linda-404@users.noreply.github.com>
2021-04-18 21:01:48 +0200

    erase hardcoded products from html

--- js/details.js
+++ js/details.js
@@ -1,7 +1,11 @@
 const detailsContainer = document.querySelector(".view-film");
 const descriptionContainer = document.querySelector(".description_container")
 const plotContainer = document.querySelector(".plot_container")
-// const title = document.querySelector("title");
+
+// const detailsContainer2 = document.querySelector(".view-film2");
+// const descriptionContainer2 = document.querySelector(".description_container2")
+// const plotContainer2 = document.querySelector(".plot_container2")
+
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
@@ -46,3 +50,39 @@ async function getFilm() {
 }
 
 getFilm();
+
+// function logged in
+
+// async function getFilm2() {
+//     detailsContainer.innerHTML = "";
+//     try {
+//         const response = await fetch(url);
+//         const results = await response.json();
+
+//         console.log(results);
+
+//         detailsContainer2.innerHTML = `<div class="view-film">
+//                                     <img src="${results.images[0].src}" class="film">
+//                                    </div>`;
+        
+//         descriptionContainer2.innerHTML = `<div class="film-description">
+//                                             <h3>Title: ${results.name}</h3>
+//                                             <h3>Genre: ${results.categories[0].name}</h3>
+//                                             <div>
+//                                                 <button class="btn">play preview</button>
+//                                                 <a href="signup2.html"><button class="btn">rent for ${results.prices.price},-</button></a>
+//                                             </div>
+//                                         </div>`;
+
+//         plotContainer2.innerHTML = `<div>
+//                                         <h3>Plot</h3>
+//                                         <p>${results.short_description}</p>
+//                                     </div>`                                
+//     }
+//     catch(error) {
+//         console.log(error);
+//         // detailsContainer.innerHTML = displayError("An error occured when calling API");
+//     }
+// }
+
+// getFilm2();
--- js/newtop.js
+++ js/newtop.js
@@ -1,6 +1,9 @@
 const newsContainer = document.querySelector(".news");
 const topRatingsContainer = document.querySelector(".top");
 
+const newsContainer2 = document.querySelector(".news2");
+const topRatingsContainer2 = document.querySelector(".top2");
+
 const url = "https://ellesdevdesigns.com/square-eyes/wp-json/wc/store/products/";
 
 
@@ -10,6 +13,7 @@ async function fetchByTag() {
         const results = await response.json();
 
         console.log(results);
+        newsContainer.innerHTML = "";
 
         for (let i = 0; i < results.length; i++) {
             console.log(results[i]);
@@ -42,6 +46,7 @@ async function fetchByRating() {
         const results = await response.json();
 
         console.log(results);
+        topRatingsContainer.innerHTML = "";
 
         for (let i = 0; i < results.length; i++) {
             console.log(results[i]);
@@ -66,3 +71,70 @@ async function fetchByRating() {
 }
 
 fetchByRating();
+
+// functions logged in
+
+async function fetchByTag2() {
+    try {
+        const response = await fetch(url + "?tag=20");
+        const results = await response.json();
+
+        console.log(results);
+        newsContainer2.innerHTML = "";
+
+        for (let i = 0; i < results.length; i++) {
+            console.log(results[i]);
+
+            newsContainer2.innerHTML += `<a href="filmdetailsloggedin.html?id=${results[i].id}" class="filminfo"> 
+                                            <img src="${results[i].images[0].src}" class="film-img">
+                                            <h4>${results[i].name}</h4>
+                                            <div class="ratings">
+                                                <i class="far fa-star"></i>
+                                                <i class="far fa-star"></i>
+                                                <i class="far fa-star"></i>
+                                                <i class="far fa-star"></i>
+                                                <i class="far fa-star"></i>
+                                                <i class="far fa-star"></i>
+                                            </div>
+                                        </a>`;
+        }
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+fetchByTag2();
+
+async function fetchByRating2() {
+    try {
+        const response = await fetch(url + "?rating=5");
+        const results = await response.json();
+
+        console.log(results);
+        topRatingsContainer2.innerHTML = ""; 
+
+        for (let i = 0; i < results.length; i++) {
+            console.log(results[i]);
+
+            topRatingsContainer2.innerHTML += `<a href="filmdetailsloggedin.html?id=${results[i].id}" class="filminfo"> 
+                                            <img src="${results[i].images[0].src}" class="film-img">
+                                            <h4>${results[i].name}</h4>
+                                            <div class="ratings">
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                            </div>
+                                        </a>`;
+        }
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+fetchByRating2();
